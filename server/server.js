79544d2dafbc1c0dceea4c702fa1e7b00e5708f6
Emma Williams <github@em01.co.uk>
2018-07-30 13:39:02 +0700

    Updates.

--- server/server.js
+++ server/server.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('./db/mongoose');
 
+
+app.get('/', (req, res) => {
+    res.send('Welcome')
+});
+
 app.listen(3000);

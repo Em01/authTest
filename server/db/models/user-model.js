d38a906bd061b71a412499c3bbfa42525ed3c2a3
Emma Williams <github@em01.co.uk>
2018-07-30 14:35:17 +0700

    Initial bcrypt implementation.

--- server/db/models/user-model.js
+++ server/db/models/user-model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const User = new mongoose.Schema({
     email: {
@@ -26,6 +27,28 @@ const User = new mongoose.Schema({
         unique: true
     }
 });
+
+
+//before we save we want to execute the pre function. This is a normal function so we can access this.
+User.pre('save', function(next) {
+    let user = this;
+
+    if (!user.isModified('password')) {
+        //if user password was not modified return next
+        return next()
+    }
+
+    bcrypt.genSalt(12, (err, salt) => {
+        if(err) {
+            return Promise.reject(err);
+        }
+
+        bcrypt.hash(user.password, salt, (err, hashedPassword) => {
+            user.password = hashedPassword;
+            next();    
+        });
+    });
+})
 
 module.exports = mongoose.model('User', User);
 //exports model and schema

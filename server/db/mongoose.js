d1942e91ddbea469b34eed3827ead41616f04a6c
Emma Williams <github@em01.co.uk>
2018-07-30 13:44:54 +0700

    Further updates

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //allows us to use promises in our mongoose code.
 
-mongoose.connect('')
+mongoose.connect('mongodb://authUser:123456a@ds159651.mlab.com:59651/auth')
 .then(() => console.log('DB Connected')
 .catch(err => console.log(err));
 

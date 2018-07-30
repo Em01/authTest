9c6a5fc91aeefdd570da959447e6b923746eb930
Emma Williams <github@em01.co.uk>
2018-07-30 13:28:18 +0700

    basic server and db setup and include gitignore.

---
+++ server/db/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+//allows us to use promises in our mongoose code.
+
+mongoose.connect('')
+.then(() => console.log('DB Connected')
+.catch(err => console.log(err));
+
+module.exports = mongoose;

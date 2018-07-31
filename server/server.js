6bf6117d3347e43e330b912c2c05831ceed29a78
Emma Williams <github@em01.co.uk>
2018-07-31 19:48:19 +0700

    Add register route.

--- server/server.js
+++ server/server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
 const mongoose = require('./db/mongoose');
-const bcrypt = require('bcrypt');
+const userRoutes = require('./routes/user-routes');
+const bodyParser = require('body-parser');
 
-let password = 'myPassword'
+app.use(bodyParser.json());
 
-let salt = bcrypt.genSaltSync(12);
-let hashedPassword = bcrypt.hashSync(password, salt);
-console.log('my hashed password is equal to -', hashedPassword);
+//not using url encoded because only making json requests
+// const bcrypt = require('bcrypt');
 
-let compare = bcrypt.compareSync(password, hashedPassword);
-console.log(compare, 'compare');
+// let password = 'myPassword'
+
+// let salt = bcrypt.genSaltSync(12);
+// let hashedPassword = bcrypt.hashSync(password, salt);
+// console.log('my hashed password is equal to -', hashedPassword);
+
+// let compare = bcrypt.compareSync(password, hashedPassword);
+// console.log(compare, 'compare');
 
 app.get('/', (req, res) => {
     res.send('Welcome')
 });
+
+app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 

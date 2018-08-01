0357c5075d15834cd108fdb8d1a522b12de31fbe
Emma Williams <github@em01.co.uk>
2018-08-01 09:01:41 +0700

    WIP: JWT and client setup.

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../db/models/user-model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', (req, res) => {
     const { email, password, username } = req.body
@@ -25,6 +27,37 @@ router.post('/register', (req, res) => {
         }
         return res.status(400).send();
     })
+});
+
+router.get('/login', (req, res) => {
+    const { username, password } = req.body;
+    User.findOne({username})
+    .then((user) => {
+        if(!user) {
+            //todo consider status codes here 401/400
+            return res.status(400).send()
+        }
+
+        bcrypt.compare(password, user.password)
+        .then(match => {
+            if(!match) {
+                return res.status(401).send();
+            }
+
+            jwt.sign({_id: user._id})
+        })
+        .catch(err => {
+            return res.status(401).semd({error: err});
+        })
+    })
+    .catch((err) => {
+        if(err) {
+            return res.status(401).send(err);
+        }
+
+        return res.status(401).send();
+    })
+
 });
 
 

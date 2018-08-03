85439e58718b00f35b2816f94c547caf4858c0f2
Emma Williams <github@em01.co.uk>
2018-08-03 09:52:08 +0700

    Sign in inputs.

--- Client/src/Screens/CreateAnAccount.js
+++ Client/src/Screens/CreateAnAccount.js
@@ -1,3 +1,4 @@
+import Validator from 'validator'
 import React, { Component } from 'react'
 
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
@@ -6,13 +7,23 @@ import CustomButton from '../Components/Button/Button';
 
 class CreateAnAccount extends Component {
 
+    state = {
+        email: '',
+        password: '',
+        username: ''
+    }
+
+    handleEmailChange = email => this.setState({ email })
+    handlePasswordChange = password => this.setState({ password })
+    handleUsernameChange = username => this.setState({ username })
+
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.signUpForm}>
-                    <Input placeholder="Email" />
-                    <Input placeholder="Username"/>
-                    <Input placeholder="Password"/>
+                    <Input placeholder="Email" value={this.state.email} onChangeText={this.handleEmailChange} />
+                    <Input placeholder="Username" value={this.state.username} onChangeText={this.handleUsernameChange}/>
+                    <Input placeholder="Password" value={this.state.password} onChangeText={this.handlePasswordChange}/>
                 </View>
                 <CustomButton text="Sign Up" />
             </View>
